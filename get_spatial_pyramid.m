dd42d044522fd2758d5e2c48f65c709a3c0667b5
thesunghwan <thesunghwan@gmail.com>
2016-12-08 21:20:53 +0900

    bag of words and nearest neighbor done

---
+++ get_spatial_pyramid.m
@@ -0,0 +1,15 @@
+function image_feats = get_spatial_pyramid(image_paths)
+% image_paths is an N x 1 cell array of strings where each string is an
+% image path on the file system.
+
+% This function assumes that 'vocab.mat' exists and contains an N x 128
+% matrix 'vocab' where each row is a kmeans centroid or visual word. This
+% matrix is saved to disk rather than passed in a parameter to avoid
+% recomputing the vocabulary every run.
+
+% image_feats is an N x d matrix, where d is the dimensionality of the
+% feature representation. In this case, d will depend on the structure of
+% the spatial pyramid you construct. 
+
+load('vocab.mat')
+vocab_size = size(vocab, 2);
